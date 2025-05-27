5f6a2bf363bd60686814fe66eb6e06468320785c
Felipe Cabrera <me@felieppe.com>
2025-05-27 20:43:22 -0300

    feat: add auth login utility

---
+++ utils/api.js
@@ -0,0 +1,22 @@
+const axios = require('axios');
+require('dotenv').config();
+
+const API_URL = process.env.API_URL || 'http://localhost:8000';
+
+async function login(email, password) {
+    const endpoint = `${API_URL}/v1/auth/login`;
+
+    try {
+        const response = await axios.post(endpoint, {
+            correo: email,
+            contraseña: password
+        });
+
+        return response.data;
+    } catch (error) {
+        console.error('Error during login:', error);
+        throw error.response ? error.response.data["error"] : error
+    }
+}
+
+export { login };
