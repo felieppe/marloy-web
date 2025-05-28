463c9f4af1a4cd72a7a73ba48fbd462309f79462
Felipe Cabrera <me@felieppe.com>
2025-05-28 00:34:10 -0300

    refactor: add slash to final endpoint to avoid redirect

--- utils/api.js
+++ utils/api.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const API_URL = process.env.API_URL || 'http://localhost:8000';
 
 async function postLogin(email, password) {
-    const endpoint = `${API_URL}/v1/auth/login`;
+    const endpoint = `${API_URL}/v1/auth/login/`;
 
     try {
         const response = await axios.post(endpoint, {
