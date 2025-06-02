6df670fe3a38a7be8e870f2c83df1434c7834e79
Felipe Cabrera <me@felieppe.com>
2025-06-02 19:21:20 -0300

    feat: add filtering of items depending on user.isAdmin

--- components/dashboardLeftMenu.jsx
+++ components/dashboardLeftMenu.jsx
@@ -1,20 +1,40 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faAddressBook, faBook, faCashRegister, faFileInvoice, faToolbox, faTruck, faUsers, faUserTie, faUtensils } from '@fortawesome/free-solid-svg-icons';
+import { useEffect, useState } from 'react';
 
 const styles = require('../styles/components/DashboardLeftMenu.module.css');
 
 function DashboardLeftMenu({ activeTab = "" }) {
     const items = [
-        { icon: faTruck, label: 'Proveedores', link: '/' },
-        { icon: faUtensils, label: 'Insumos', link: '/insumos' },
-        { icon: faAddressBook, label: 'Clientes', link: '/clientes' },
-        { icon: faCashRegister, label: 'Maquinas', link: '/maquinas' },
-        { icon: faUserTie, label: 'Tecnicos', link: '/tecnicos' },
-        { icon: faToolbox, label: 'Mantenimientos', link: '/mantenimientos' },
-        { icon: faFileInvoice, label: 'Registro de consumo', link: '/consumos' },
-        { icon: faUsers, label: 'Usuarios', link: '/usuarios' },
-        { icon: faBook, label: 'Reportes', link: '/reportes' }
+        { icon: faTruck, label: 'Proveedores', link: '/', adminRequired: true },
+        { icon: faUtensils, label: 'Insumos', link: '/insumos', adminRequired: false },
+        { icon: faAddressBook, label: 'Clientes', link: '/clientes', adminRequired: false },
+        { icon: faCashRegister, label: 'Maquinas', link: '/maquinas', adminRequired: true },
+        { icon: faUserTie, label: 'Tecnicos', link: '/tecnicos', adminRequired: true },
+        { icon: faToolbox, label: 'Mantenimientos', link: '/mantenimientos', adminRequired: false },
+        { icon: faFileInvoice, label: 'Registro de consumo', link: '/consumos', adminRequired: false },
+        { icon: faUsers, label: 'Usuarios', link: '/usuarios', adminRequired: true },
+        { icon: faBook, label: 'Reportes', link: '/reportes', adminRequired: true }
     ]
+
+    const [showItems, setShowItems] = useState([]);
+
+    useEffect(() => {
+        const user = JSON.parse(localStorage.getItem('user') || '{}');
+        if (!user) return;
+
+        switch (user.isAdmin) {
+            case true:
+                setShowItems(items);
+                break;
+            case false:
+                setShowItems(items.filter(item => !item.adminRequired));
+                break;
+            default:
+                setShowItems([]);
+                break;
+        }
+    }, [])
 
     return (
         <div className={styles.menu}>
@@ -23,7 +43,7 @@ function DashboardLeftMenu({ activeTab = "" }) {
             </div>
 
             <div className={styles.menu__items}>
-                {items.map((item, index) => (
+                {showItems.map((item, index) => (
                     <a key={index} href={item.link} className={(styles.menu__item + ' ' + (activeTab === item.label ? styles.item__active : ''))}>
                         <FontAwesomeIcon icon={item.icon} className={styles.icon} style={{fontSize: '20px'}}/>
                         <span className={styles.item__label}>{item.label}</span>
